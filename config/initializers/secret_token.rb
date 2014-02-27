e02621202295e5ba42e9cf8288c621fb05038260
DPL-1 <DPL-1@DPL-1s-iMac.local>
2014-02-27 13:43:38 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FaceSpace::Application.config.secret_key_base = '185004207e60feb6e040c5566eb7c215ef8f91caae2335434904a2c44a41ebe74dc9fd42c1ed90414f96652605bcc3e3a01620202f05606a2c8b3af76084bb62'
