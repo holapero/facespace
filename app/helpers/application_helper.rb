290db2a160e55212f4e78da210c73a842a372bf7
DPL-1 <DPL-1@DPL-1s-iMac.local>
2014-02-27 16:03:58 -0800

    working copy with sign in and sign out

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
+require 'digest/md5'
+
 module ApplicationHelper
+  def gravatar_url(email, options={})
+    digest = Digest::MD5.hexdigest(email)
+    url_options = {host: "www.gravatar.com/avatar/#{digest}", d: "retro"}.merge(options)
+    url = url_for(url_options)
+    url
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 FaceSpace::Application.routes.draw do
+  get "users/index"
   devise_for :users
   root "static#index"
 end
